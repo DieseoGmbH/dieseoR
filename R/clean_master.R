f8a0fcb9d8fff21676f48c67ce753528d718aaa3
andresdies <andres.dieseo@gmail.com>
2026-03-06 10:31:20 +0100

    again updated clean_master and description, update to version 0.1.2

--- R/clean_master.R
+++ R/clean_master.R
@@ -1,14 +1,15 @@
-#' Lese Daten ein und führe Standard-Säuberungen durch
+#' Lese Daten ein und/oder führe Standard-Säuberungen durch
 #'
-#' Diese Funktion nimmt einen Dateipfad, liest den entsprechenden Datensatz
-#' (RDS, CSV oder Excel) ein und führt direkt Standard-Bereinigungen durch.
+#' Diese Funktion nimmt entweder einen Dateipfad oder direkt einen Data Frame.
+#' Wenn ein Pfad übergeben wird, liest sie den entsprechenden Datensatz
+#' (RDS, CSV oder Excel) ein. Anschließend führt sie direkte Standard-Bereinigungen durch:
 #' Die Spaltennamen werden mittels \code{janitor::clean_names()} standardisiert.
 #' Zudem werden alle Textspalten (Character) von überschüssigen Leerzeichen
 #' befreit und in Kleinbuchstaben umgewandelt.
 #'
-#' @param input Character. Der komplette Pfad zur Datei, die eingelesen werden soll.
-#' @param type Character. Das Format der Datei. Unterstützt werden "rds" (Standard), "csv", "excel" (oder "xlsx"/"xls").
-#' @param ... Weitere Argumente, die an die jeweilige Einlese-Funktion (z. B. \code{read.csv} oder \code{readxl::read_excel}) weitergegeben werden sollen.
+#' @param input Character oder Data Frame. Der komplette Pfad zur Datei, die eingelesen werden soll, oder ein bereits geladener Data Frame.
+#' @param type Character. Das Format der Datei, falls ein Pfad übergeben wird. Unterstützt werden "rds" (Standard), "csv", "excel" (oder "xlsx"/"xls").
+#' @param ... Weitere Argumente, die an die jeweilige Einlese-Funktion (z. B. \code{utils::read.csv} oder \code{readxl::read_excel}) weitergegeben werden sollen.
 #'
 #' @return Ein bereinigter Data Frame (bzw. Tibble).
 #' @export
@@ -21,34 +22,42 @@
 #'
 #' @examples
 #' \dontrun{
-#' # RDS-Datei einlesen
+#' # 1. RDS-Datei einlesen und bereinigen
 #' df_rds <- clean_master("data/meine_daten.rds")
 #'
-#' # CSV-Datei einlesen (mit base R utils::read.csv)
-#' df_csv <- clean_master("data/rohdaten.csv", type = "csv", sep = ";", dec = ",")
-#'
-#' # Excel-Datei einlesen (liest nur das Tabellenblatt "Umsatz")
-#' df_excel <- clean_master("data/finanzen.xlsx", type = "excel", sheet = "Umsatz")
+#' # 2. Bereits geladenen Data Frame nur bereinigen
+#' mein_df <- data.frame(Name_Spalte = c("  Test ", "HALLO  "))
+#' df_clean <- clean_master(mein_df)
 #' }
 clean_master <- function(input, type = "rds", ...) {
 
-  # 1. Um Fehler abzufangen, falls jemand "CSV" groß schreibt
-  file_type <- tolower(type)
+  # 1. Prüfen, ob input ein Data Frame oder ein Dateipfad ist
+  if (is.data.frame(input)) {
+    # Wenn es schon ein Data Frame ist, einfach übernehmen und Einlesen überspringen
+    df <- input
 
-  # 2. Datei einlesen, abhängig vom Typ
-  if (file_type == "rds") {
-    df <- readRDS(input)
+  } else if (is.character(input)) {
+    # 2. Wenn es ein Pfad ist, Datei einlesen abhängig vom Typ
+    file_type <- tolower(type)
 
-  } else if (file_type == "csv") {
-    # Nutzt base R (utils) anstelle von readr
-    df <- utils::read.csv(input, ...)
+    if (file_type == "rds") {
+      df <- readRDS(input)
 
-  } else if (file_type %in% c("excel", "xlsx", "xls")) {
-    # Benötigt weiterhin das readxl-Package
-    df <- readxl::read_excel(input, ...)
+    } else if (file_type == "csv") {
+      # Nutzt base R (utils) anstelle von readr
+      df <- utils::read.csv(input, ...)
+
+    } else if (file_type %in% c("excel", "xlsx", "xls")) {
+      # Benötigt weiterhin das readxl-Package
+      if (!requireNamespace("readxl", quietly = TRUE)) stop("Bitte installiere das 'readxl' Paket.")
+      df <- readxl::read_excel(input, ...)
+
+    } else {
+      stop("Nicht unterstützter Dateityp. Bitte 'rds', 'csv' oder 'excel' verwenden.")
+    }
 
   } else {
-    stop("Nicht unterstützter Dateityp. Bitte 'rds', 'csv' oder 'excel' verwenden.")
+    stop("'input' muss entweder ein Dateipfad (Character) oder ein Data Frame sein.")
   }
 
   # 3. Die Datenbereinigung
